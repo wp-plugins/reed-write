e3c451d96be30d97237010072cc8929b01e5372b
iambriansreed <iambriansreed@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-07 00:36:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reed-write/trunk@434393 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ _rw_script.js.php
@@ -0,0 +1,27 @@
+<?php
+
+	require ( current(explode('wp-content', dirname(__FILE__))).'wp-load.php' );
+	header("Content-type: text/javascript");
+	if(0){ ?><script type="text/javascript"><?php };
+?>
+var site_root = '<?php echo get_bloginfo('wpurl'); ?>';
+var _rw_taxonomies = <?php echo json_encode($rw_taxonomies); ?>;
+var _rw_content_types = <?php echo json_encode($rw_content_types); ?>;
+var _rw_query = function(){ };
+jQuery(function($){
+	_rw_query = function(query, _function){
+		var json = null;
+		
+		$.ajax({
+			type: 'POST',
+			data: query,
+			url:'<?php echo plugins_url('/_rw_query.php', __FILE__); ?>',
+			success: function(data){ json = data; },
+			async : false
+		});
+		
+		if(typeof(_function) != 'function')
+			return json;
+		_function(json); return true;
+	}
+});
