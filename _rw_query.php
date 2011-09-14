20c300ca41a99c6e1cdb82b1950e591e83cf4224
iambriansreed <iambriansreed@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-14 00:57:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reed-write/tags/1.2.0@437807 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ _rw_query.php
@@ -0,0 +1,42 @@
+<?php
+
+	require ( current(explode('wp-content', dirname(__FILE__))).'wp-load.php' );
+	
+	$query = array(
+		'showposts' => 5,
+		'post_type' => 'project',
+		'meta_query' => array(
+			array(
+				'key' => 'featured',
+				'value' => '',
+				'compare' => '!='
+			)
+		)
+	);
+	
+	$_rw_query = _rw_query_posts($query);
+	
+/*
+$posts = query_posts($query);
+
+echo '<pre>'.print_r($posts, 1).'</pre>';
+
+*/
+
+	header('Cache-Control: no-cache, must-revalidate');
+	header('Content-type: application/json');
+	
+	$rows = array();
+	
+	$posts = query_posts($_POST);
+	
+	if ( !count($posts) ) { echo '[]'; exit; }
+	unset($post);
+	foreach($posts as $post){
+		$post->post_content = _rw_get_the_content_with_formatting();
+		$rows[] = _rw_get_post($post->ID);
+	}
+	
+	echo json_encode( $rows );
+	
+?>
